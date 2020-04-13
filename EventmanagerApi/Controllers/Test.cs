1299a02690cd4ee35cc778dbc5dde1e87e8e2e17
sdrozd97 <sebastian.drozd97@gmail.com>
2020-04-13 14:49:47 +0200

    feature: configure swagger

---
+++ EventmanagerApi/Controllers/Test.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventmanagerApi.Controllers
+{
+	public class Test : ControllerBase
+	{
+		[HttpGet("api/user")]
+		public IActionResult Get()
+		{
+			return Ok(new {Name = "Seba"});
+		}
+	}
+}
--- EventmanagerApi/Startup.cs
+++ EventmanagerApi/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventmanagerApi.Data;
+using EventmanagerApi.Options;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace EventmanagerApi
 {
@@ -25,7 +29,15 @@ namespace EventmanagerApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<DataContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 			services.AddControllers();
+
+			services.AddSwaggerGen(x =>
+			{
+				x.SwaggerDoc("v1", new OpenApiInfo {Title = "Eventmanager API", Version = "v1"});
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +47,12 @@ namespace EventmanagerApi
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			var swaggerOptions = new SwaggerOptions();
+			Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+			app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+			app.UseSwaggerUI(option => { option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description); });
 
 			app.UseHttpsRedirection();
 
